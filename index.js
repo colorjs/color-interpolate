dd1aeba086cd9c66159ac48506cf1e2fe3d7851a
wangxiao <wangxiao@patsnap.com>
2016-10-19 15:30:19 +0800

    use es5

--- index.js
+++ index.js
@@ -11,36 +11,37 @@ const clamp = require('mumath/clamp');
 module.exports = interpolate;
 
 function interpolate (palette) {
-	palette = palette.map(c => {
+	palette = palette.map(function(c) {
 		c = parse(c);
 		if (c.space != 'rgb') {
-			if (c.space != 'hsl') throw `${c.space} space is not supported.`;
+			if (c.space != 'hsl') throw 'c.space' + 'space is not supported.';
 			c.values = hsl.rgb(c.values);
 		}
 		c.values.push(c.alpha);
 		return c.values;
 	});
 
-	return (t, mix = lerp) => {
+	return function(t, mix) {
+		mix = mix || lerp;
 		t = clamp(t, 0, 1);
 
-		let idx = ( palette.length - 1 ) * t,
+		var idx = ( palette.length - 1 ) * t,
 			lIdx = Math.floor( idx ),
 			rIdx = Math.ceil( idx );
 
 		t = idx - lIdx;
 
-		let lColor = palette[lIdx], rColor = palette[rIdx];
+		var lColor = palette[lIdx], rColor = palette[rIdx];
 
-		let result = lColor.map((v, i) => {
+		var result = lColor.map(function(v, i) {
 			v = mix(v, rColor[i], t);
 			if (i < 3) v = Math.round(v);
 			return v;
 		});
 
 		if (result[3] === 1) {
-			return `rgb(${result.slice(0,3)})`;
+			return 'rgb('+result.slice(0,3)+')';
 		}
-		return `rgba(${result})`;
+		return 'rgba('+result+')';
 	};
 }
